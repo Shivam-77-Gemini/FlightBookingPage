a641a5a9eda061ad41a89505e6cbd338e280b86b
Shivam-77-Gemini <Shivam.Tiwari@Geminisolutions.com>
2022-05-20 17:42:14 +0530

    Change

---
+++ FlightBookingPage/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { BookingPageComponent } from './booking-page/booking-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginFormComponent,
+    BookingPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
